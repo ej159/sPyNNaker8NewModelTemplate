6b3df24fb832e9280ca69e831b2641ad4837e111
ej159 <edwardgeorgejones@gmail.com>
2021-01-27 16:53:14 +0000

    Add necessary files for zlif implementation

---
+++ python_models8/neuron/builds/zlif_curr_delta.py
@@ -0,0 +1,41 @@
+# A PyNN Model for standard neurons built from components
+from spynnaker.pyNN.models.neuron import AbstractPyNNNeuronModelStandard
+
+# Components from main tools
+from spynnaker.pyNN.models.neuron.input_types import InputTypeDelta
+from spynnaker.pyNN.models.neuron.synapse_types import SynapseTypeDelta
+from spynnaker.pyNN.models.neuron.threshold_types import ThresholdTypeStatic
+
+# Additional components
+from python_models8.neuron.neuron_models.neuron_model_zero_leak_integrate_and_fire import NeuronModelZeroLeakIntegrateAndFire
+from spynnaker.pyNN.models.defaults import default_initial_values
+
+
+class ZLIFCurrDelta(AbstractPyNNNeuronModelStandard):
+
+    # Identify which of the values are state variables
+    @default_initial_values({"v", "isyn_exc", "isyn_inh"})
+    def __init__(
+            self, cm=1.0, v_rest=0.0, v_reset=0.0,
+            v_thresh=64.0, tau_refrac=0.0, i_offset=0.0, v=0.0,
+            isyn_exc=0.0, isyn_inh=0.0):
+
+        neuron_model = NeuronModelZeroLeakIntegrateAndFire(
+            v, v_rest, cm, i_offset, v_reset, tau_refrac)
+        synapse_type = SynapseTypeDelta(isyn_exc, isyn_inh)
+        input_type = InputTypeDelta()
+        threshold_type = ThresholdTypeStatic(v_thresh)
+
+
+        # Create the model using the superclass
+        super(ZLIFCurrDelta, self).__init__(
+
+            # the model a name (shown in reports)
+            model_name="ZIFCurrDelta",
+
+            # the matching binary name
+            binary="ZIF_curr_delta.aplx",
+
+            # the various model types
+            neuron_model=neuron_model, input_type=input_type,
+            synapse_type=synapse_type, threshold_type=threshold_type)
